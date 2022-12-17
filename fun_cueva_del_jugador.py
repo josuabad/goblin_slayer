6e0c00e1d3e135f1f909b31f58339cee14ec4ad7
Josu Abad <josu.abad@alumno.ucjc.edu>
2022-12-17 11:06:35 +0100

    En desarrollo: inventario

--- fun_cueva_del_jugador.py
+++ fun_cueva_del_jugador.py
@@ -21,7 +21,22 @@ def inventario(mochila, mochila_sin_uso=0):  # La mochila es una lista de cosas 
         print(f'Tienes {len(mochila_sin_uso)} guardas:\n')
         for cosas in range(len(mochila_sin_uso)):
             print(f'{cosas + 1}. {mochila_sin_uso[cosas]}')
+    seguir = True
+    while seguir:
+        nuevo = input('¿Deseas añadir o o coger algo? [A/C]: ')
+        if nuevo.lower() == 'a':
+            num = int(input('¿Cuantas cosas vas a añadir?: '))
+        elif nuevo.lower() == 'c':
+            return 'Vuelve cuando quieras'  # No va a realizar ninguna operación dentro del inventario
+        else:
+            print('Opción incorrecta')
+            error = input('¿Quieres salir o seguir en el inventario? [S/I]: ')
+            if error.lower() == 's':
+                seguir = False
+            elif error.lower() == 'i':
+                break
+            else:
+                print('Opción no encontrada')
+                continue
     
-    # ¿Deseas añadir algo? ...
     # Que sí: cuantas cosas, cuales cosas (desde la mochila), las añades, devuelve nuevas cosas guardadas.
-    # Que no: te vas a ir a la puerta de tu cueva a ver qué haces, devuelve False.
