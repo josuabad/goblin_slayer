b43d9eecfa3eabddca71ba97d78e6aaf7e07923d
Diego Matías <diegoreinoso892@gmail.com>
2022-12-19 10:20:05 +0100

    añadida la funcion de extraccion de elementos de un diccionario

---
+++ lib/fun_extraccion_dict.py
@@ -0,0 +1,12 @@
+def extraer(diccionario, from_key, extrae_value_key, extrae_valor=True):
+    # diccionario       ----    Diccionario
+    # from_key          ----    Cuál es la palabra clave desde dónde buscar
+    # extrae_value_key  ----    Atributo a buscar de la palabra clave
+    # extrae_valor      ----    True = extrae valor; False = muestra una lista de los items con el atributo seleccionado
+    lista_keys = list(diccionario.keys())
+    lista_values = list(diccionario.values())
+    if extrae_valor:
+        return lista_values[lista_keys.index(from_key)].get(extrae_value_key)
+    else:
+        for items, num in zip(lista_keys, range(len(lista_keys))):
+            print(f'- {items} | ({lista_values[num].get(extrae_value_key)})')
