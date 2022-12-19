e6b417e9afb84145a11473bae662d5c795dbf018
Josu Abad <josu.abad@alumno.ucjc.edu>
2022-12-19 14:40:38 +0100

    Algunos cambios

--- lib/fun_extraccion_dict.py
+++ lib/fun_extraccion_dict.py
@@ -12,8 +12,7 @@ def extraer(diccionario, from_key, extrae_value_key, extrae_valor=True):
             print(f'- {items} | ({lista_values[num].get(extrae_value_key)})')
 
 
-"""
-def modificar(diccionario_emisor, diccionario_receptor, from_key, atributo, valor_de_cambio):  # Devuelve el diccionario usado y el modificado
+def modificar(diccionario_emisor, from_key, atributo, valor_de_cambio, diccionario_receptor=False, cambiar=False):  # La función cambia valores (True), pero también puede modificarlos de un diccionario a otro (False)(Ejemplo: de mochila a inventario)
     # Quiero saber los atributos de un from_key que no se dónde se encuentra.
     diccionario_emisor_view_keys = list(diccionario_emisor.keys())
     diccionario_emisor_view_values = list(diccionario_emisor.values())
@@ -27,18 +26,24 @@ def modificar(diccionario_emisor, diccionario_receptor, from_key, atributo, valo
     que_cambiar = list(diccionario_emisor_view_values[donde_encontrar].values())
     # Desarrollo
     valor_de_origen = extraer(diccionario_emisor, from_key, atributo)
-    if valor_de_cambio < valor_de_origen and valor_de_cambio > 0:  # Caso 1
-        diccionario_receptor.update({diccionario_emisor_view_keys[donde_encontrar]: {donde_buscar[donde_cambiar]: valor_de_cambio}})  # 1º Metes en el inventario todo lo que quieres valor_de_cambio
-        # Pasar todos los atributos restantes del from_key
-        donde_buscar.pop(donde_cambiar)
-        que_cambiar.pop(donde_cambiar)
-        for elmnt in donde_buscar:
-            diccionario_receptor[diccionario_emisor_view_keys[donde_encontrar]][elmnt] = que_cambiar[donde_buscar.index(elmnt)]
-        # Borrar el sobrante de la diccionario_emisor
-        # cantidad_original = extraer(diccionario_emisor, 'espada', 'cantidad')
-        diccionario_emisor[diccionario_emisor_view_keys[donde_encontrar]]['cantidad'] = cantidad_original - valor_de_cambio
-    elif valor_de_cambio == valor_de_origen:  # Caso 2
-        diccionario_receptor.update({diccionario_emisor_view_keys[donde_encontrar]: {donde_buscar[donde_cambiar]: valor_de_cambio}})  # 1º Metes en el inventario todo lo que quieres valor_de_cambio
-    else:  # Caso 3. Se podría usar un try/except
-        pass
-"""
+    if type(valor_de_cambio) == int:
+        if valor_de_cambio < valor_de_origen and valor_de_cambio > 0:
+            # 1º Cambiar el atributo           
+            diccionario_receptor.update({diccionario_emisor_view_keys[donde_encontrar]: {donde_buscar[donde_cambiar]: valor_de_cambio}})
+            # 2º Pasar el resto de atributos
+            donde_buscar.pop(donde_cambiar)
+            que_cambiar.pop(donde_cambiar)
+            for elmnt in donde_buscar:
+                diccionario_receptor[diccionario_emisor_view_keys[donde_encontrar]][elmnt] = que_cambiar[donde_buscar.index(elmnt)]
+            # 3º Borrar el sobrante del anterior
+            diccionario_emisor.update({diccionario_emisor_view_keys[donde_encontrar]: {donde_buscar[donde_cambiar]: valor_de_origen - valor_de_cambio}})
+            return diccionario_emisor, diccionario_receptor
+        elif valor_de_cambio == valor_de_origen:
+            diccionario_receptor.update({diccionario_emisor_view_keys[donde_encontrar]: {donde_buscar[donde_cambiar]: valor_de_cambio}})
+            diccionario_emisor.pop(from_key)
+            return diccionario_emisor, diccionario_receptor
+        else:  # Hay que soportar el error desde el programa
+            pass
+    else:
+        diccionario_emisor.update({diccionario_emisor_view_keys[donde_encontrar]: {donde_buscar[donde_cambiar]: valor_de_cambio}})
+        return diccionario_emisor
