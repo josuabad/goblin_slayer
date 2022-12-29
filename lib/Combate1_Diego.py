b9cb06430d26c615740761dd18252e0c96b32c91
Diego Matías <diegoreinoso892@gmail.com>
2022-12-29 22:48:59 +0100

    Primera Version de combate y otros cambios sin importancia

--- lib/Combate1_Diego.py
+++ lib/Combate1_Diego.py
@@ -1,18 +1,72 @@
 #Librerias de otros ficheros/archivos
 from funcionesjueguito import *
+#Variables globales de prueba Jugador (mochila y stats)
+stats_player = {"vida":22,"destreza":11,"fuerza":11}
+mochila = {"arma":{"puño":0}}
 
 #Variables Globales de los enemigos
 Combate = False
 Ratas = False
 Goblins = False
 Ogro = False
-Estaferno = False
+Estafermo = False
+turno_player = False
+turno_enemigo = False
 
 
+def combate_estafermo(inventario,player_stats):
+    vida_estafermo = 50000
+    dam_estafermo = 0
+    impacto_estafermo = 0 #Lo que debe sacar para impactar al enemigo
+    golpe_estafermo = 0 #Lo que debe sacar el estafermo para impactar al jugador
+    Combate = True
+    while Combate == True:
+        golpe_player = dados(20)
+        print("Es tu turno Jugador")
+        if golpe_player >= impacto_estafermo:
+            turno_player = True
+        else:
+            print("No has impactado")
+            turno_enemigo = True
 
 
+        while turno_player == True:
+            print("Has impactado")
+            dam_player = (stats_player.get("destreza") - 10) + dados(4)
+            vida_estafermo = vida_estafermo-dam_player
+            if vida_estafermo <= 0:
+                print("El estafermo ha quedado destruido,has terminado el entrenamiento.")
+                Combate,turno_player = False,False
+                break
+            print(f"El Estafermo tiene {vida_estafermo} de vida.")
+            decision = input("¿Quieres seguir luchando?  SI||NO: ")
+            decision = decision.upper()
+            if decision == "NO":
+                print("Bien, has decidido huir has terminado tu sesion de entrenamiento")
+                Combate = False
+                break
+            else:
+                pass
+            turno_player,turno_enemigo = False,True
 
-def combate(inventario, player_stats, enemigo_selec):
+        while turno_enemigo == True:
+            print("\nEs el turno del Estafermo")
+            impacto_player = 12 + (stats_player.get("destreza")-10)
+            golpe_estafermo = dados(20)
+
+            if golpe_estafermo >= impacto_player:
+                print("El estafermo ha impactado")
+                vida_player = stats_player.get("vida")
+                vida_player = vida_player - dam_estafermo
+                stats_player.update({"vida":vida_player})
+                print(f"El Estafermo te ha hecho {dam_estafermo} punto de daño tienes, {vida_player} puntos de vida")
+                turno_player,turno_enemigo = True,False
+            else:
+                print("El Estafermo no ha impactado")
+                turno_enemigo,turno_player = False,True
+
+
+def combate(inventario, stats_player, enemigo_selec):
     enemigo = enemigo_selec
     if enemigo == (Ratas == True):
         print("ratas es True")
@@ -20,18 +74,13 @@ def combate(inventario, player_stats, enemigo_selec):
         print("goblins")
     elif enemigo == (Ogro == True):
         print("ogro")
-    elif enemigo == (Estaferno == True):
+    elif enemigo == (Estafermo == True):
         print("estaferno")
-        #combate_estaferno(inventario,player_Stats)
+        combate_estafermo(inventario,stats_player)
 
+Estafermo = True
 
-
-def combate_estaferno(inventario,player_stats):
-    vida_estaferno = 50000
-    dam_estaferno = 0
-    Combate = True
-    while Combate == True:
-
+combate(mochila,stats_player,Estafermo)
 
 
 
--- lib/funcionesjueguito.py
+++ lib/funcionesjueguito.py
@@ -1,8 +1,6 @@
 import random
 
-vida_rata = 0
-damage_rata = 0
-damage = 0
+
 
 
 def dados(n): #La función en resumen crea una lista según el número de caras del dado y de ahi hace un random.choice
@@ -17,27 +15,9 @@ def dados(n): #La función en resumen crea una lista según el número de caras 
 
 
 
-def rat_attack():
-    damage_rata = dados(4)
-    damage = damage_rata
-    return damage_rata
-
-def gob_attack():
-    damage_gob = dados(4)
-    damage = damage_rata
-    return damage_rata
 
 
-#Manera de calcular el daño para cualquier enemigo V1
-vida_player = 20
-print(vida_player)
-vida_player2 = vida_player-(rat_attack())
-print(f"La rata ha hecho,{20-vida_player2} puntos de daño ")
-print(vida_player2)
-#Para la huida, creamos una variable que guarde el resultado de la eleccion como un SI o un NO
-print("Has iniciado un combate, si deseas huir debes sacar más de 6 en un dado de 10. ")
-
-
+"""
 try:
     eleccion_huida = input(" SI|NO : ")
     eleccion_huida = eleccion_huida.upper()
@@ -54,8 +34,7 @@ if eleccion_huida == "SI" or "1":
     else:
         print(f"Has sacado un {huida}.")
         print("ooohh..... no, no has logrado huir te va a tocar luchar")
-
-
+"""
 
 
 
