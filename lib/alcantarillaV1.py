88ebc9d13206029fbc004fed286fcc34a050a83f
Diego Matías <diegoreinoso892@gmail.com>
2022-12-29 02:12:51 +0100

    Creado archivo alcantarillas y primeras pruebas

---
+++ lib/alcantarillaV1.py
@@ -0,0 +1,25 @@
+"""print("Has entrado a las alcantarillas")
+
+ratas = False
+def alcantarilla(inventario,player_stats): #llamaremos a la primera funcion que es la alcantarilla principal
+    ratas = True #Activamos el identificador del enemigo
+    a = inventario
+    b = player_stats
+    enemigo = ratas
+    def combate(a,b,enemigo):
+        if enemigo == (ratas == True):
+            print("Ratas")
+        else:
+            print("No ratas")
+        
+alcantarilla(12,3)"""
+
+def d(b,c):
+    print(b,c)
+
+def a(b,c):
+    print(b,c)
+    print (d(c,b))
+
+
+a(1,2)
