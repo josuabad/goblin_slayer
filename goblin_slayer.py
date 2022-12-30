59592d024a797093e4432707e2381252fde1f2e8
Josu Abad <josu.abad@alumno.ucjc.edu>
2022-12-30 21:32:48 +0100

    Correción de errores y optimización final

--- goblin_slayer.py
+++ goblin_slayer.py
@@ -1,49 +1,6 @@
 # Versión oficial
-import time
 from lib import features
-from lib import fun_puntos
-
-
-def habilidades(habilidades, puntos):
-    if puntos == 0:
-        print('No tienes puntos de habilidad disponibles')
-        print('Sigue jugando para conseguir mejoras y subir de nivel')
-        time.sleep(3)
-        features.borrar_pantalla
-        return habilidades, puntos
-    else:
-        while True:
-            print(f'Tienes {puntos} puntos disponibles para tus habilidades (fuerza/destreza/constitución)')
-            decision_1 = input('¿Deseas repartirlos? [s/n]: ')
-            if decision_1.lower() == 's':
-                while True:
-                    habilidad = input('¿Qué habilidad vas a mejorar? [f/d/c]: ')
-                    num_puntos = int(input('¿Cuántos puntos vas a utilizar?: '))
-                    if habilidad.lower() == 'f' and puntos >= 0 and num_puntos <= puntos:
-                        reparto = fun_puntos.nuevo_punto(habilidades, 'fuerza', num_puntos, puntos)
-                        puntos = reparto[1]
-                        return habilidades, puntos
-                    elif habilidad.lower() == 'd' and puntos >= 0 and num_puntos <= puntos:
-                        reparto = fun_puntos.nuevo_punto(habilidades, 'destreza', num_puntos, puntos)
-                        puntos = reparto[1]
-                        return habilidades, puntos
-                    elif habilidad.lower() == 'c' and puntos >= 0 and num_puntos <= puntos:
-                        reparto = fun_puntos.nuevo_punto(habilidades, 'constitucion', num_puntos, puntos)
-                        puntos = reparto[1]
-                        return habilidades, puntos
-                    elif num_puntos > puntos:
-                        print('No puedes usar más puntos de los que tienes')
-                        print('Por favor, repita el proceso con una cantidad coherente')
-                    else:
-                        features.borrar_pantalla()
-                        print('Algo ha ido mal, repita el proceso por favor')
-                        continue
-            elif decision_1.lower() == 'n':
-                return habilidades, puntos
-            else:  # Soporte de errores
-                features.borrar_pantalla
-                print('Algo ha ido mal, repita el proceso por favor')
-                continue
+from lib import config_habilidades
 
 
 # Desarrollo
@@ -56,11 +13,12 @@ print(f'¡Bienvenido {nombre}!')
 
 # Habilidades del personaje
 personaje_habilidades = {'fuerza': 10, 'destreza': 10, 'constitucion': 10}
-dano = personaje_habilidades['fuerza'] - 10
-defensa = personaje_habilidades['destreza'] - 10
-vida = 20 + (2 * (personaje_habilidades['constitucion'] - 10))
 puntos_habilidades = 3
 
 # Creación del personaje
-cambio = habilidades(personaje_habilidades, puntos_habilidades)
-puntos_habilidades = cambio[1]
+set_habilidades = config_habilidades.habilidades((personaje_habilidades, puntos_habilidades))
+puntos_habilidades = set_habilidades[1]  # Actualización de los puntos de habilidad restantes
+set_personaje = config_habilidades.especificas_personaje(personaje_habilidades)
+dano = set_personaje[0]
+defensa = set_personaje[1]
+vida = set_personaje[2]
---
+++ lib/combate_goblins.py
@@ -0,0 +1,51 @@
+import random
+import features
+# Puedes huir en dos puntos; antes de entrar o dentro de la cueva
+# Si no has entrado en la cueva hay que sacar >= 5 en 20. Si no, >= 10 + numero de enemigos en 20
+def huir(num, inicio=True):
+    dado = random.randint(0, 20)
+    if inicio:
+        if dado >= 5:
+            return True
+        else:
+            return False
+    else:
+        if dado >= 10 + num:
+            return True
+        else:
+            return False
+
+
+def combate_goblins():
+    # Generar enemigos (num y clase)
+    features.borrar_pantalla()
+    num_enemigos = random.randint(1, 6)
+    clase = ['espada', 'escudo']
+    clase_enemigo = []
+    if num_enemigos == 1:
+        print(f'¡Cuidado! Hay {num_enemigos} enemigo dentro de la cueva')
+    else:
+        print(f'¡Cuidado! Hay {num_enemigos} enemigos dentro de la cueva')
+    for enemigo in range(num_enemigos):
+        tipo_clase = random.choice(clase)
+        print(f'{enemigo + 1}. Goblin con {tipo_clase}')
+        clase_enemigo.append(tipo_clase)
+    num_escudo = clase_enemigo.count('escudo')
+    num_espada = clase_enemigo.count('espada')
+    if num_escudo < num_enemigos or num_espada < num_enemigos:
+        print('-----  Total  -----')
+        print(f'Goblins con espada: {num_espada}')
+        print(f'Goblins con escudo: {num_escudo}')
+    while True:
+        lucha = input('¿Quieres huir antes de entrar en combate? [s/n]: ')
+        if lucha.lower() == 's':
+            salida = huir(num_enemigos)
+            if salida:
+                return  # Tiene que devolver algo que sea lo mismo que se le mete a la función
+            else:
+                break
+        elif lucha.lower() == 'n':
+            break
+        else:
+            print('Algo ha ido mal, repita el proceso por favor')
+            continue
