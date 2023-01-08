ea9fa75ab846fbbfa2f0c60e04c3d83d39e15b69
Josu Abad <josu.abad@alumno.ucjc.edu>
2023-01-08 13:02:34 +0100

    Terminado función medidor_de_bolsas

--- lib/features.py
+++ lib/features.py
@@ -2,14 +2,14 @@ import os
 import time
 
 
-def borrar_pantalla():  # El código para borrar la pantalla depende del SO
+def borrar_pantalla():  # Borra la pantalla
     if os.name == "posix":
         os.system("clear")
     elif os.name == "ce" or os.name == "nt" or os.name == "dos":
         os.system("cls")
 
 
-def pantalla_de_carga():  # Embellecedor
+def pantalla_de_carga():  # Embellecedor de tiempo de carga
     tiempo = 0
     while tiempo != 3:
         borrar_pantalla()
@@ -23,3 +23,34 @@ def pantalla_de_carga():  # Embellecedor
         time.sleep(1)
         borrar_pantalla()
         tiempo += 1
+
+    
+def medidor_de_bolsas(bolsillo):
+    # Explicación
+    # Mira cuantas cosas tienes, sin tener en cuenta las monedas.
+    # Devuelve True si hay espacio, False en caso contrario (20 elementos en total)
+
+    # Desarrollo
+    cantidad_total = 0
+    cantidad_monedas = 0
+    for elemento in bolsillo:
+        try:
+            cantidad_total += bolsillo[elemento]["cantidad"]
+        except KeyError:
+            cantidad_monedas = bolsillo[elemento]
+    if cantidad_total == 0:
+        print('Todavía no tienes nada, sal a la aventura y cárgate con lo que te encuentres')
+        print('El el futuro te puede ser de gran ayuda')
+        print(f'Tienes {cantidad_monedas} monedas')
+        return True
+    elif 9 <= cantidad_total <= 11:
+        print(f'Tienes tus bolsillos llenos hasta la mitad ({cantidad_total})')
+        print('Considera ir a tu cueva a guardar los excedentes')
+        print('O ve a la tienda y véndelos a cambio de monedas de oro para comprar nuevas armas, protección, o pociones')
+        print(f'Tienes {cantidad_monedas} monedas')
+        return True
+    elif cantidad_total == 20:
+        print('Tienes que ir a tu cueva a dejar todo lo que has guardado')
+        print('¡Ya no te entra nada en los bolsillos!')
+        print(f'Tienes {cantidad_monedas} monedas')
+        return False
