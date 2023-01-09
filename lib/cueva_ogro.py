aa2552aef5313b00a868b96ef3dffeab81b73e52
Diego Matías <diegoreinoso892@gmail.com>
2023-01-09 01:47:26 +0100

    Creado archivo Cueva_Ogro

---
+++ lib/cueva_ogro.py
@@ -0,0 +1,4 @@
+from funcionesjueguito import *
+
+stats_player = {"vida": 22,"const":1,"destreza":11,"fuerza":11,}
+mochila = {"Armas": {"puño": 0, "espada": 3, "Escudo": 4, "Espada_Magica": 1}, "objetos":{"Pociones":3, "Hidromiel": 0, "Monedas": 4, "Llave": True}}
